0a6396f21bb37005c26360f5e93e608ddee9ea8c
MisakaDev <31675199+MisakaDev@users.noreply.github.com>
2018-09-10 04:17:08 +0300

    Add new permissions

---
+++ ProjectVillageCore/permissions.py
@@ -0,0 +1,11 @@
+import copy
+
+from rest_framework import permissions
+
+
+class ExtendedDjangoModelPermission(permissions.DjangoModelPermissions):
+    def __init__(self):
+        self.perms_map = copy.deepcopy(self.perms_map)
+        self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
+        self.perms_map['OPTIONS'] = ['%(app_label)s.view_%(model_name)s']
+        self.perms_map['HEAD'] = ['%(app_label)s.view_%(model_name)s']
--- ProjectVillageCore/settings.py
+++ ProjectVillageCore/settings.py
@@ -70,7 +70,7 @@ CORS_ORIGIN_WHITELIST = (
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'ProjectVillageCore.permissions.ExtendedDjangoModelPermission',
     ),
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
