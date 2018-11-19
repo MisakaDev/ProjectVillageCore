f838ceb2da1976397e3d2f59a1b18fbf062f4a78
MisakaDev <31675199+MisakaDev@users.noreply.github.com>
2018-11-19 22:50:45 +0200

    Added permission list in auth response

--- auth/views.py
+++ auth/views.py
@@ -6,4 +6,5 @@ class Login(LoginView):
         response = super().get_response()
         response.data['id'] = self.user.id
         response.data['username'] = self.user.username
+        response.data['permission'] = [perm.name for perm in self.user.user_permissions.all()]
         return response
