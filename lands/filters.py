106dcf19540dfe5e5c3382746a39dcb86f27f497
MisakaDev <31675199+MisakaDev@users.noreply.github.com>
2018-11-24 23:55:45 +0200

    Extend search fields

--- lands/filters.py
+++ lands/filters.py
@@ -1,16 +1,22 @@
 from django_filters.rest_framework import FilterSet, NumberFilter, ModelChoiceFilter
 
 from persons.models import Person
-from .models import LandPlot, LandPurpose, LandOwn, LandRent
+from .models import LandPlot, LandPurpose, LandOwn, LandRent, LandZone
 
 
 class LandPlotFilter(FilterSet):
     area = NumberFilter(field_name='area')
     purpose = ModelChoiceFilter(queryset=LandPurpose.objects.all())
+    quarter__zone = ModelChoiceFilter(queryset=LandZone.objects.all())
+    code = NumberFilter()
+    quarter__code = NumberFilter()
+    quarter__zone__code = NumberFilter()
+    quarter__zone__unit__code = NumberFilter()
 
     class Meta:
         model = LandPlot
-        fields = ('area', 'purpose')
+        fields = ('area', 'purpose', 'quarter__zone', 'code', 'quarter__code', 'quarter__zone__code',
+                  'quarter__zone__unit__code')
 
 
 class LandOwnFilter(FilterSet):
